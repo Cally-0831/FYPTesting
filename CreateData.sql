d64006dec3a8a84bb2ca11261c60efe4b213b75a
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-07-03 12:07:18 +0800

    checking

---
+++ CreateData.sql
@@ -0,0 +1,76 @@
+
+delimiter |
+
+CREATE TRIGGER testref BEFORE INSERT ON class
+  FOR EACH ROW
+  BEGIN
+  declare stringstring  varchar(8000);
+  declare countcount integer;
+  set stringstring = new.cid;
+  set countcount =0;
+  select count(*) into countcount from class 
+  where rid = new.rid and weekday = new.weekday and (starttime <= new.starttime and endtime >= new.starttime);
+  if countcount >0 then
+    set new.rid ="empty";
+    END IF;
+  END;
+  |
+delimiter ;
+
+
+
+##delimiter |
+##CREATE TRIGGER checkstudenttakecourse BEFORE INSERT ON student_take_course
+##  FOR EACH ROW
+##  BEGIN
+##  declare stringstring  varchar(8000);
+##  declare countcount integer;
+##  set stringstring = new.cid;allusersallusers
+##  set countcount =0;
+##  select count(*) into countcount from student_take_course 
+##  where cid = new.cid and sid = new.sid;
+##  if countcount >0 then
+##    insert into logs values("duplicated");
+##    END IF;
+##  END;
+##  |
+##delimiter ;
+
+delimiter |
+CREATE TRIGGER addalluserstoroletable BEFORE INSERT ON allusers
+  FOR EACH ROW
+  BEGIN
+##  declare stringstring  varchar(8000);
+##  declare countcount integer;
+##  set stringstring = new.cid;allusersallusers
+
+##  set countcount =0;
+##  select count(*) into countcount from student_take_course 
+##  where cid = new.cid and sid = new.sid;
+  if new.role = "stu" then
+    insert into student values(new.allusersname,new.pid,new.password,new.states,new.errortime);
+	elseif new.role = "sup" then
+    insert into supervisor values(new.allusersname,new.pid,new.password,new.states,new.errortime);
+    END IF;
+  END;
+  |
+delimiter ;
+
+
+insert into allusers 	values("Apple","sid11111","spw11111","active","0","stu");
+insert into allusers values("Betty","sid22222","spw22222","active","0","stu");
+
+
+insert into class values("COMP1005","00001","COMP100500001","RM101","MON","08:30","09:20");
+insert into class values("COMP1005","10001","COMP100510001","RM101","MON","08:30","09:20");
+insert into class values("COMP1005","10002","COMP100510002","RM101","TUE","09:00","09:20");
+insert into class values("COMP1005","10003","COMP100510003","RM101","MON","09:30","10:20");
+
+insert into student_take_course 	values("COMP100500001","111");
+insert into classroom 	values("RM101");
+insert into classroom 	values("RM102");
+
+INSERT INTO  allusers VALUES ("Prof Chan","tid00001",tpw00001,"Active",0,"sup");
+INSERT INTO  allusers VALUES ("Prof Lam","tid00002",tpw00002,"Active",0,"sup");
+
+INSERT INTO  student_take_course VALUES("s01","c01");
---
+++ CreateTables.sql
@@ -0,0 +1,60 @@
+
+
+DROP TABLE IF EXISTS allusers;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS supervisor;
+DROP TABLE IF EXISTS class;
+DROP TABLE IF EXISTS classroom;
+DROP TABLE IF EXISTS logs;
+DROP TABLE IF EXISTS student_take_course;
+DROP trigger IF exists testref;
+Drop trigger if exists checkstudenttakecourse;
+
+create table  allusers(
+allusersname	varchar(100) Not null,
+pid			varchar(20) not null,
+password	varchar(20) not null,
+states		varchar(20),
+errortime	int,
+role	varchar(20),
+PRIMARY key (pid));
+
+create table   student(
+stdname		varchar(100) Not null,
+sid			varchar(20) not null,
+password	varchar(20) not null,
+states		varchar(20),
+errortime	int,
+PRIMARY key (sid));
+
+create table supervisor(
+supname		varchar(100) Not null,
+tid			varchar(20) not null,
+password	varchar(20) not null,
+states		varchar(20),
+errortime	int,
+PRIMARY key (tid));
+
+create table class(
+CCode		varchar(10) Not null,
+CSecCode	varchar(10) not null,
+CID			varchar(20) not null,
+RID			varchar(10) not null,
+weekday 	varchar(10) not null,
+startTime	time,
+endTime		time,
+Check (endTime>startTime),
+PRIMARY key (CID));
+
+create table classroom(
+RID		varchar(10) Not null,
+PRIMARY key (RID));
+
+create table logs(logstring varchar(8000));
+
+create table Student_take_Course(
+CID		varchar(20) not null,
+SID		varchar(10) not null,
+CONSTRAINT csid
+primary key (CID,SID)
+);
