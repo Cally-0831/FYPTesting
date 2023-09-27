c5ad0e54b02a056228e457711488d36e6b34ebdc
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-09-27 12:36:00 +0800

    getting best timing

--- emptyFYP/api/controllers/ScheduleController.js
+++ emptyFYP/api/controllers/ScheduleController.js
@@ -163,16 +163,16 @@ module.exports = {
 
         var startday = new Date(req.body.fullstartday);
         var startime = startday.toLocaleTimeString("en-GB");
-        var endday =  new Date(req.body.fullendday);
+        var endday = new Date(req.body.fullendday);
         var endtime = endday.toLocaleTimeString("en-GB");
-        console.log((endday - startday)/1000/60/60/24)
-        var sessionduration=0;
+        console.log((endday - startday) / 1000 / 60 / 60 / 24)
+        var sessionduration = 0;
         var typeofpresent = req.body.typeofpresent;
-        
+
         if (typeofpresent == "midterm") {
-sessionduration = 20;
+            sessionduration = 20;
         } else {
-sessionduration = 60;
+            sessionduration = 60;
         }
 
 
@@ -199,66 +199,131 @@ sessionduration = 60;
                                 for (var a = 0; a < superttb.length; a++) {
                                     supweeklist[parseInt(superttb[a].weekdays) - 1].push(superttb[a]);
                                 }
-
-                                var getobsttb = "select * from allsupertakecourse left join allclass on allsupertakecourse.CID = allclass.CID where allsupertakecourse.pid = \"" + req.body.oid + "\" and confirmation = \"1\" order by weekdays asc, startTime asc"
-                                //console.log(getobsttb)
-                                db.query(getobsttb, (err, results) => {
+                                var getsupschedulebox = "select * from allschedulebox where tid = \"" + req.body.tid + "\" order by boxdate asc , boxtime asc";
+                                db.query(getsupschedulebox, (err, results) => {
                                     try {
                                         var string = JSON.stringify(results);
                                         var json = JSON.parse(string);
-                                        var obsttb = json;
-                                        for (var a = 0; a < obsttb.length; a++) {obsweeklist[parseInt(obsttb[a].weekdays) - 1].push(obsttb[a]);}
-                                        var getstdttb = "select * from allstudenttakecourse left join allclass on allstudenttakecourse.CID = allclass.CID where allstudenttakecourse.pid = \"" + req.body.sid + "\" and confirmation = \"2\" order by weekdays asc, startTime asc"
-                                        //console.log(getstdttb)
-                                        db.query(getstdttb, (err, results) => {
+                                        var superschedulebox = json;
+                                        var getobsttb = "select * from allsupertakecourse left join allclass on allsupertakecourse.CID = allclass.CID where allsupertakecourse.pid = \"" + req.body.oid + "\" and confirmation = \"1\" order by weekdays asc, startTime asc"
+                                        //console.log(getobsttb)
+                                        db.query(getobsttb, (err, results) => {
                                             try {
                                                 var string = JSON.stringify(results);
                                                 var json = JSON.parse(string);
-                                                var stdttb = json;
-
-                                                for (var a = 0; a < stdttb.length; a++) {stdweeklist[parseInt(stdttb[a].weekdays) - 1].push(stdttb[a]);}
-                                                for (var a = 0; a < stdweeklist.length; a++) {
-                                                    if (stdweeklist[a].length == 0) { stdweeklist[a].push("EMPTY") }
-                                                    if (obsweeklist[a].length == 0) { obsweeklist[a].push("EMPTY") }
-                                                    if (supweeklist[a].length == 0) { supweeklist[a].push("EMPTY") }
-                                                }
-                                                var getsuppreference = "select allpreffromsup.tid , priority, prefno  from allpreffromsup  left join supervisor on supervisor.tid = allpreffromsup.tid where allpreffromsup.tid = \"" + req.body.tid + "\" or allpreffromsup.tid = \"" + req.body.oid + "\" order by priority asc"
-                                                db.query(getsuppreference, (err, results) => {
+                                                var obsttb = json;
+                                                for (var a = 0; a < obsttb.length; a++) { obsweeklist[parseInt(obsttb[a].weekdays) - 1].push(obsttb[a]); }
+                                                var getobsschedulebox = "select * from allschedulebox where tid = \"" + req.body.oid + "\" order by boxdate asc , boxtime asc";
+                                                db.query(getobsschedulebox, (err, results) => {
                                                     try {
                                                         var string = JSON.stringify(results);
                                                         var json = JSON.parse(string);
-                                                        var suppref = json;
-                                                        console.log(suppref)
+                                                        var obsschedulebox = json;
+                                                        var getstdttb = "select * from allstudenttakecourse left join allclass on allstudenttakecourse.CID = allclass.CID where allstudenttakecourse.pid = \"" + req.body.sid + "\" and confirmation = \"2\" order by weekdays asc, startTime asc"
+                                                        //console.log(getstdttb)
+                                                        db.query(getstdttb, (err, results) => {
+                                                            try {
+                                                                var string = JSON.stringify(results);
+                                                                var json = JSON.parse(string);
+                                                                var stdttb = json;
 
-                                                        var startindex = startday.getDay();
-
-                                                        for(var daynum = 0 ; daynum < Math.floor((endday - startday)/1000/60/60/24);a++){
-                                                            var presentday = new Date(startday.getTime() + (daynum*86400000)); 
-                                                            var currentsessiontimeinpresentday = presentday.toLocaleTimeString("en-GB");
-                                                            var currentsessionendtimeinpresentday = presentday.getTime()+(1000*60*sessionduration)
-                                                            for(var timebox = 0 ;timebox < 20 ; timebox++){
-
-                                                            }
-                                                            for(var a = 0 ;a < superttb.length;a++){
-                                                                if(!(superttb[a].weekdays == presentday.getDay() && !(superttb[a].startime >= currentsessionendtimeinpresentday || currentsessiontimeinpresentday >= superttb[a].endtime))){
-                                                                    
-                                                                }else{
-                                                                    console.log(currentsessionendtimeinpresentday,superttb[a])
+                                                                for (var a = 0; a < stdttb.length; a++) { stdweeklist[parseInt(stdttb[a].weekdays) - 1].push(stdttb[a]); }
+                                                                for (var a = 0; a < stdweeklist.length; a++) {
+                                                                    if (stdweeklist[a].length == 0) { stdweeklist[a].push("EMPTY") }
+                                                                    if (obsweeklist[a].length == 0) { obsweeklist[a].push("EMPTY") }
+                                                                    if (supweeklist[a].length == 0) { supweeklist[a].push("EMPTY") }
                                                                 }
-                                                            }
-                                                            
-                                                        }
-                                                        
+                                                                var getsuppreference = "select allpreffromsup.tid , priority, prefno  from allpreffromsup  left join supervisor on supervisor.tid = allpreffromsup.tid where allpreffromsup.tid = \"" + req.body.tid + "\" or allpreffromsup.tid = \"" + req.body.oid + "\" order by priority asc"
+                                                                db.query(getsuppreference, (err, results) => {
+                                                                    try {
+                                                                        var string = JSON.stringify(results);
+                                                                        var json = JSON.parse(string);
+                                                                        var suppref = json;
+                                                                        //console.log(suppref)
+
+                                                                        var startindex = startday.getDay();
+
+                                                                        for (var daynum = 0; daynum < Math.floor((endday - startday) / 1000 / 60 / 60 / 24); daynum++) {
+                                                                            var presentday = new Date(startday.getTime() + (daynum * 86400000));
+                                                                            var currentsessiontimeinpresentday = presentday.toLocaleTimeString("en-GB");
+                                                                            var currentsessionendtimeinpresentday = (new Date(presentday.getTime() + (1000 * 60 * sessionduration))).toLocaleTimeString("en-GB");
+
+                                                                            for (var timebox = 0; timebox < 12; timebox++) {
+                                                                                var fitcase;
+                                                                                var checker = 0;
+                                                                                for (var a = 0; a < superschedulebox.length; a++) {
+                                                                                    if (superschedulebox[a].boxdate == presentday.toLocaleDateString("en-GB") && superschedulebox[a].boxtime == currentsessiontimeinpresentday) {
+                                                                                        checker = -1;
+                                                                                        break;
+                                                                                    } else { checker = 1; }
+                                                                                }
+                                                                                
+                                                                                if (checker >= 0) {
+                                                                                    for (var a = 0; a < obsschedulebox.length; a++) {
+                                                                                        if (obsschedulebox[a].boxdate == presentday.toLocaleDateString("en-GB") && obsschedulebox[a].boxtime == currentsessiontimeinpresentday) {
+                                                                                            checker = -1;
+                                                                                            break;
+                                                                                        } else { checker = 1; }
+                                                                                    }
+                                                                                }
+                                                                               
+                                                                                if (checker >= 0) {
+                                                                                    for (var a = 0; a < superttb.length; a++) {
+                                                                                        if ((superttb[a].weekdays == presentday.getDay() && (superttb[a].startime >= currentsessionendtimeinpresentday || currentsessiontimeinpresentday >= superttb[a].endtime))) {
+                                                                                            checker = -1;
+                                                                                            break;
+                                                                                        } else {
+                                                                                            checker = 1;
+                                                                                        }
+                                                                                    }
+                                                                                }
+
+                                                                                if (checker >= 0) {
+                                                                                    for (var a = 0; a < obsttb.length; a++) {
+                                                                                        if ((obsttb[a].weekdays == presentday.getDay() && (obsttb[a].startime >= currentsessionendtimeinpresentday || currentsessiontimeinpresentday >= obsttb[a].endtime))) {
+                                                                                            checker = -1;
+                                                                                            break;
+                                                                                        } else {
+                                                                                            checker = 1;
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                                if (checker >= 0) {
+                                                                                    for (var a = 0; a < stdttb.length; a++) {
+                                                                                        if ((stdttb[a].weekdays == presentday.getDay() && (stdttb[a].startime >= currentsessionendtimeinpresentday || currentsessiontimeinpresentday >= stdxttb[a].endtime))) {
+                                                                                            checker = -1;
+                                                                                            break;
+                                                                                        } else {
+                                                                                            checker = 1;
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                                if(checker >=0 ){
+                                                                                    console.log("this is a good timeslot");
+                                                                                    console.log(presentday.toLocaleString());
+                                                                                }
+                                                                                presentday.setHours(presentday.getHours() + 1);
+                                                                                currentsessiontimeinpresentday = presentday.toLocaleTimeString("en-GB");
+                                                                                currentsessionendtimeinpresentday = (new Date(presentday.getTime() + (1000 * 60 * sessionduration))).toLocaleTimeString("en-GB");
+                                                                            }
+
+
+                                                                        }
 
 
 
 
-                                                        return res.ok();
-                                                    } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.getsuppref") }
+
+                                                                        return res.ok();
+                                                                    } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.getsuppref") }
+                                                                })
+                                                            } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.stdttb") }
+                                                        })
+                                                    } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.getobsschedulebox") }
                                                 })
-                                            } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.stdttb") }
+                                            } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.obsttb") }
                                         })
-                                    } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.obsttb") }
+                                    } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.fixedgetclassinfo.getsupschedulebox") }
                                 })
                             } catch (err) { return res.status(401).json("Error happened when excuting ScheduleController.createschedule.supttb") }
                         })
