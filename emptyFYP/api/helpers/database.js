989dff05522ce6fcfa4d4c1860f1868a8815a7f2
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-11-30 23:23:48 +0800

    11 Source Code guidelines

--- emptyFYP/api/helpers/database.js
+++ emptyFYP/api/helpers/database.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Psycho.K0831",
-    database: "fyptesting"
+    password: "Password",
+    database: "databasename",
 });
 db.connect(async (err) => {
     if (err) {
