9659a2012b94576fb7e8883b664866a31f502ebf
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-09 11:31:58 +0800

    try deployment

---
+++ emptyFYP/config/locals.js
@@ -0,0 +1,10 @@
+module.exports = {
+    port: 80,
+    environment: 'production',
+    adapters: {
+        mysql: {
+            user: 'root',
+            password: 'Psycho.K0831'
+        }
+    }
+}
