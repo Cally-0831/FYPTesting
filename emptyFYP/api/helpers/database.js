16b689d5dba9fb9cd4fa63248c9275d3315121ad
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-14 16:06:19 +0800

    try

--- emptyFYP/api/helpers/database.js
+++ emptyFYP/api/helpers/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var db = mysql.createConnection({
     host: "fypdeploy-mysql",
     //host: "localhost",
-    user: "root",
+    user: "fypdeploy",
     port: 3306,
     password: "Psycho.K0831",
     database: "fypdeploy"
