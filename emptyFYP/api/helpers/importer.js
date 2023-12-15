c90d26b7d9fd43703fb83d1bf7a317a7d95c7c0e
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-15 18:29:06 +0800

    try

--- emptyFYP/api/helpers/importer.js
+++ emptyFYP/api/helpers/importer.js
@@ -1,8 +1,9 @@
 
-const host = 'localhost';
+const host = 'localhost:3306';
 const user = 'root';
 const password = 'Psycho.K0831';
 const database = 'fypdeploy';
+const port = "3306"
 
 const Importer = require('mysql2-import');
 const importer = new Importer({host, user, password, database});
