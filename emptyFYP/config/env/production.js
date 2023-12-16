f40bebf2195c3fd4b358a457e823671dcf949084
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-16 20:54:50 +0800

    try

--- emptyFYP/config/env/production.js
+++ emptyFYP/config/env/production.js
@@ -194,7 +194,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     adapter: '@sailshq/connect-redis',
-    url: 'redis://root:Psycho.K0831@fypdeploy-mysql:3306/fypdeploy',
+    url: 'redis://fypdeploy-mysql:3306/fypdeploy',
     //--------------------------------------------------------------------------
     // /\   OR, to avoid checking it in to version control, you might opt to
     // ||   set sensitive credentials like this using an environment variable.
