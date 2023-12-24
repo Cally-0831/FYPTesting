9590bb6b29a5a08ce394020975564cb26d4dd7f1
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-24 08:49:27 +0800

    sql

--- SQL/Standard/AllclassSQL.sql
+++ SQL/Standard/AllclassSQL.sql
@@ -74,11 +74,7 @@ insert into alltakecourse values("COMP4096_00001","tid00001");
 insert into alltakecourse values("COMP4025_00001","tid00001");
 insert into alltakecourse values("COMP4025_10001","tid00001");
 
-insert into alltakecourse values("COMP1005_00001","tid00003");
-insert into alltakecourse values("COMP1005_10201","tid00003");
-insert into alltakecourse values("COMP1005_10101","tid00003");
-insert into alltakecourse values("COMP1005_10102","tid00003");
-
+insert into alltakecourse values("COMP1015_00001","tid00003");
 insert into alltakecourse values("COMP4107_00001","tid00002");
 insert into alltakecourse values("COMP2027_00001","tid00002");
 insert into alltakecourse values("COMP2035_00001","tid00002");
