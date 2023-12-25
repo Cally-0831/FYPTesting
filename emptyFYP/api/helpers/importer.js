673fb8cee8acee5cd241c903c2d665ac3008ab0d
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-26 00:11:06 +0800

    try

--- emptyFYP/api/helpers/importer.js
+++ emptyFYP/api/helpers/importer.js
@@ -8,7 +8,7 @@ const port = 3306;
 
 const Importer = require('mysql2-import');
 //const importer = new Importer({host, user, password});
-const importer = new Importer({host, user, password,database,timezone});
+const importer = new Importer({host, user, password,database});
 module.exports = {
 
     fn: async function () {
