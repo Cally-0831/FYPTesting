95154ca80c0772ab273da1503654a9f4d0b2ace9
Cally-0831 <19222068@life.hkbu.edu.hk>
2023-12-31 22:50:26 +0800

    db set back to fyp1

--- emptyFYP/api/helpers/importer.js
+++ emptyFYP/api/helpers/importer.js
@@ -1,6 +1,6 @@
 
-const host = 'fypdeploy2-mysql';
-//const host = 'fypdeploy-mysql';
+//const host = 'fypdeploy2-mysql';
+const host = 'fypdeploy-mysql';
 const user = 'root';
 const password = 'Psycho.K0831';
 const database = 'fypdeploy';
