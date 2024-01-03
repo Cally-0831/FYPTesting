6e75cdd4253d89110c5f1751049a0d196a680e9d
Cally-0831 <19222068@life.hkbu.edu.hk>
2024-01-03 14:58:29 +0800

    deploy to 2

--- emptyFYP/api/helpers/database2.js
+++ emptyFYP/api/helpers/database2.js
@@ -1,8 +1,8 @@
 const { createPool } = require("mysql2")
 
 const pool = createPool({
-   host: "fypdeploy3-mysql",
-//    host: "fypdeploy2-mysql",
+//    host: "fypdeploy3-mysql",
+   host: "fypdeploy2-mysql",
    //host: "fypdeploy-mysql",
 
     //host: "localhost",
